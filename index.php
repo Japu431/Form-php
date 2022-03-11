ebb06e783c5aaa388666636870a2b22159ee86e7
Japu431 <japamnk@gmail.com>
2022-03-11 10:44:33 -0300

    Form sem BD

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    if (!isset($_SESSION['login'])) {
+
+        if (isset($_POST['acao'])) {
+
+            // Simula BD
+            $login = "Matheus";
+            $senha = "12345";
+
+
+            $loginForm = $_POST['login'];
+            $senhaForm = $_POST['senha'];
+
+            if ($login == $loginForm && $senha == $senhaForm) {
+                // Logado com sucesso!
+
+                $_SESSION['login'] = $login;
+                header('Location: index.php');
+            } else {
+                echo 'Dados inválidos!';
+            }
+        }
+
+        include('login.php');
+    } else {
+        if(isset($_GET['logout'])) {
+            unset($_SESSION['login']);
+            session_destroy();
+            header('Location: index.php');
+        }
+        include('home.php');
+    }
+    ?>
+</body>
+
+</html>
